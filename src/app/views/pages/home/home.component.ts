c1ca4d99543436962d1600514b0e7d51fb3a3188
Joel Tengco <joeltengco3510@gmail.com>
2024-09-24 18:20:52 -0700

    Fix scroll issue from nav page

--- src/app/views/pages/home/home.component.ts
+++ src/app/views/pages/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -15,7 +14,6 @@ export class HomeComponent {
   }[];
 
   constructor(
-    private router: Router,
   ) {
 
     this.pageThumbnailDetails = [
@@ -40,9 +38,5 @@ export class HomeComponent {
         navigateToPath: 'credits',
       },
     ];
-  }
-
-  navigateToPage(path: string) {
-    this.router.navigateByUrl(path);
   }
 }
